c236911c9e5a99c621197bc6e8d8c601894ef59a
Yaktanov <Yaktanov@gmail.com>
2018-11-11 21:57:03 +0600

    First commit

--- src/Input.java
+++ src/Input.java
@@ -3,15 +3,26 @@ import java.util.Scanner;
 public class Input {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите имя: ");
+        while (true) {
+            System.out.println("Введите имя: ");
             String name = scanner.nextLine();
-        System.out.println("Введите фамилию: ");
+
+            System.out.println("Введите фамилию: ");
             String surname = scanner.nextLine();
-        System.out.println("Введите возраст: ");
+
+            System.out.println("Введите возраст: ");
             int age = scanner.nextInt();
-        System.out.println("Пол: ");
+
+            System.out.println("Пол: ");
             char c = scanner.next(".").charAt(0);
 
-        System.out.println(name + " " + surname + ", " + age + " лет" + ", " + "Пол: " + c + ", ");
+            String s = scanner.nextLine();
+            if (s == "stop") {
+                break;
+            }
+            System.out.println(name + " " + surname + ", " + age + " лет" + ", " + "Пол: " + c + ", ");
+
+
+        }
     }
 }
